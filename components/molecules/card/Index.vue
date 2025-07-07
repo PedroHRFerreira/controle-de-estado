bc0082c706156fff683453d1828deac34ed85d51
PedroHRFerreira <devpedrohr@gmail.com>
2025-07-07 10:29:23 -0300

    feat: :sparkles: start do card

---
+++ components/molecules/card/Index.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default defineComponent({
+  name: "MoleculesCard",
+});
+</script>
+
+<template>
+  <aside>
+    <img src="/images/placeholder.png" alt="placeholder" />
+    <div>
+      <AtomsText
+        size="large"
+        types="primary"
+        text="Card Title"
+        @onclick="() => console.log('Card title clicked')"
+      />
+    </div>
+  </aside>
+</template>
+
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
