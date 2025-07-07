591bd887e4b094d6c23cdf90cba6d5e019af9420
PedroHRFerreira <devpedrohr@gmail.com>
2025-07-07 12:45:32 -0300

    feat: :sparkles: store montada

--- store/sales/useSalesStore.ts
+++ store/sales/useSalesStore.ts
@@ -3,6 +3,92 @@ import { defineStore } from "pinia";
 export const useSalesStore = defineStore("sales", {
   state: () => ({
     cards: [],
+    cart: [],
     selectedCard: null,
   }),
+
+  actions: {
+    setCards(cards) {
+      this.$state.cards = cards;
+    },
+    setCart(cart) {
+      this.$state.cart = cart;
+    },
+    setSelectedCard(card) {
+      this.$state.selectedCard = card;
+    },
+    async fetchCards() {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+
+      const cards = [
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+          remove: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+          remove: false,
+        },
+      ];
+      this.setCards(cards);
+    },
+  },
 });
