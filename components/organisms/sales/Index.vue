80516a0714790432e3a3991d38f464470d02722e
PedroHRFerreira <devpedrohr@gmail.com>
2025-07-07 11:26:21 -0300

    feat: :sparkles: start na tela de vendas

---
+++ components/organisms/sales/Index.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+export default defineComponent({
+  name: "OrganismsSales",
+  setup() {
+    const cards = computed(() => {
+      return [
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+        },
+      ];
+    });
+
+    return {
+      cards,
+    };
+  },
+});
+</script>
+
+<template>
+  <article>
+    <MoleculesCard
+      :cards="cards"
+      @on-click="() => console.log('Card clicked')"
+    />
+  </article>
+</template>
+
+<style scoped lang="scss">
+@use "styles.module.scss" as *;
+</style>
