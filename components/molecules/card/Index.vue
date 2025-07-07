0e102a9cf0dd55c1c7587b16434149405b739a7e
PedroHRFerreira <devpedrohr@gmail.com>
2025-07-07 11:02:00 -0300

    feat: :sparkles: botão do card adicionado

--- components/molecules/card/Index.vue
+++ components/molecules/card/Index.vue
@@ -8,9 +8,15 @@ export default defineComponent({
   <aside class="card">
     <img src="/images/sales.jpeg" alt="placeholder" />
     <hr class="card--divider" />
-    <div>
+    <div class="card__content">
       <AtomsText size="medium" text="Card Title" />
-      <AtomsText size="extra-small" text="Card Title" />
+      <AtomsText
+        size="extra-small"
+        text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+      />
+      <button class="card--button" @click="$emit('on-click')">
+        <AtomsText size="extra-small" text="Card Title" />
+      </button>
     </div>
   </aside>
 </template>
