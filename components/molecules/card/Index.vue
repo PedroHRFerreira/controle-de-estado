2317fd77eba8b59d95b0d453a5126e3c863fade2
PedroHRFerreira <devpedrohr@gmail.com>
2025-07-07 11:20:17 -0300

    feat: :sparkles: trasformando os card em arrays

--- components/molecules/card/Index.vue
+++ components/molecules/card/Index.vue
@@ -1,21 +1,49 @@
 <script lang="ts">
 export default defineComponent({
   name: "MoleculesCard",
+  setup() {
+    const cards = computed(() => {
+      return [
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: false,
+        },
+        {
+          title: "Card Title",
+          description:
+            "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: "/images/sales.jpeg",
+          disabled: true,
+        },
+      ];
+    });
+
+    return {
+      cards,
+    };
+  },
 });
 </script>
 
 <template>
-  <aside class="card">
-    <img src="/images/sales.jpeg" alt="placeholder" />
+  <aside v-for="(card, index) in cards" :key="index" class="card">
+    <img :src="card.image" alt="imagem de um produto" />
     <hr class="card--divider" />
     <div class="card__content">
-      <AtomsText size="medium" text="Card Title" />
-      <AtomsText
-        size="extra-small"
-        text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-      />
-      <button class="card--button" @click="$emit('on-click')">
-        <AtomsText size="extra-small" text="Card Title" />
+      <AtomsText size="medium" :text="card.title" />
+      <AtomsText size="extra-small" :text="card.description" />
+      <button
+        :class="
+          card.disabled
+            ? 'card__content--button--disabled'
+            : 'card__content--button'
+        "
+        @click="$emit('on-click')"
+      >
+        <AtomsText size="extra-small" text="Adicionar no carrinho" />
       </button>
     </div>
   </aside>
